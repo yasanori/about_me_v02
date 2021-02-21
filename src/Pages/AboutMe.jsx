3e1882ae74f76f335d4892e7c832bbcf69da90cc
nagao <38156084+yasanori@users.noreply.github.com>
2021-02-21 11:52:25 +0900

    about me

--- src/Pages/AboutMe.jsx
+++ src/Pages/AboutMe.jsx
@@ -1,10 +1,75 @@
 import React from 'react'
+import { SubTitle, MiniTitle } from '../Styles/Text'
+import { AboutMePageWrapper, ProfileWrapper, Profiles, ProfileLists, ProfileList } from '../Styles/Pages/AboutMe'
 
 const AboutMe = () => {
   return (
-    <div>
-      <h1>AboutMe</h1>
-    </div>
+    <AboutMePageWrapper>
+      <SubTitle>
+        about me
+    </SubTitle>
+      <ProfileWrapper>
+        <MiniTitle>
+          Profile
+        </MiniTitle>
+        <Profiles>
+          <ProfileLists>
+            <ProfileList>
+              名前　　　:　永尾 優典
+            </ProfileList>
+            <ProfileList>
+              生年月日　:　1995-06-18
+            </ProfileList>
+            <ProfileList>
+              所在地　　:　都京都内
+            </ProfileList>
+            <ProfileList>
+              お問い合わせ:　msnr.nagao@gmail.com
+            </ProfileList>
+            <ProfileList>
+              スキル：　　<br />
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              Ruby&nbsp;/&nbsp;Ruby on Rails<br />
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              JavaScript&nbsp;<br />
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              React&nbsp;/&nbsp;Redux<br />
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              HTML5&nbsp;/&nbsp;CSS3<br />
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              デザイン&nbsp;/&nbsp;フォトグラフ<br />
+            </ProfileList>
+          </ProfileLists>
+          <ProfileLists>
+            <ProfileList>
+              name　 : Masanori Nagao
+            </ProfileList>
+            <ProfileList>
+              birthday: 1995-06-18
+            </ProfileList>
+            <ProfileList>
+              location : Tokyo, Japan
+            </ProfileList>
+            <ProfileList>
+              mail :　msnr.nagao@gmail.com
+            </ProfileList>
+            <ProfileList>
+              skill：　　<br />
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              Ruby&nbsp;/&nbsp;Ruby on Rails<br />
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              JavaScript&nbsp;<br />
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              React&nbsp;/&nbsp;Redux<br />
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              HTML5&nbsp;/&nbsp;CSS3<br />
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              design&nbsp;/&nbsp;photograph<br />
+            </ProfileList>
+          </ProfileLists>
+        </Profiles>
+      </ProfileWrapper>
+    </AboutMePageWrapper>
   )
 }
 
---
+++ src/Styles/Pages/AboutMe.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+import { COLORS, FONT_SIZE, PAGE_WRAPPER } from '../StyleContents';
+import { TEXT_BASE } from '../Text';
+
+export const AboutMePageWrapper = styled.main`
+  width: PAGE_WRAPPER.WIDTH;
+  padding: 10% 25%;
+
+`
+export const ProfileWrapper = styled.section`
+    box-sizing: border-box;
+    width: 100%;
+    padding: 10% 15%;
+`
+
+export const Profiles = styled.div`
+    box-sizing: border-box;
+  width: 100%;
+  display: flex;
+    padding: 5% 0;
+  justify-content: space-between;
+`
+
+export const ProfileLists = styled.ul`
+
+`
+
+export const ProfileList = styled.li`
+  ${TEXT_BASE}
+  font-size: 0.8rem;
+  font-weight: 300;
+`
