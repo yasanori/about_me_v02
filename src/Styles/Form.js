4e21bc04d7166850fadcce957b1f1d723e853442
nagao <38156084+yasanori@users.noreply.github.com>
2021-03-04 07:51:08 +0900

    form  レスポンス

--- src/Styles/Form.js
+++ src/Styles/Form.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
 import { COLORS, FONT_SIZE } from './StyleContents';
+import { sp, pc } from '../Styles/media';
 
 export const FormWrapper = styled.div`
   box-sizing: border-box;
--- src/Styles/Pages/Contact.js
+++ src/Styles/Pages/Contact.js
@@ -2,9 +2,16 @@ import styled from 'styled-components';
 import { COLORS, FONT_SIZE, PAGE_WRAPPER } from '../StyleContents';
 import { TEXT_BASE } from '../Text';
 import { LINK_BASE } from '../Link';
+import { sp, pc } from '../../Styles/media';
+
 
 export const ContactPageWrapper = styled.main`
   box-sizing: border-box;
   width: ${PAGE_WRAPPER.WIDTH};
-  padding: 10% 25%;
+  ${pc`
+    padding: 10% 25%;
+  `}
+  ${sp`
+    padding: 10% 0;
+  `}
 `
