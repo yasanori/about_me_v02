38212ff513ff11a32fc9de8d1f68ab8b2d0341e9
nagao <38156084+yasanori@users.noreply.github.com>
2021-03-04 07:50:53 +0900

    comming soon レスポンス

--- src/Styles/Pages/Work.js
+++ src/Styles/Pages/Work.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import { COLORS, PAGE_WRAPPER } from '../StyleContents';
 import { TEXT_BASE } from '../Text';
+import { sp, pc } from '../../Styles/media';
 
 export const WorkPageWrapper = styled.main`
   box-sizing: border-box;
@@ -17,4 +18,9 @@ export const CommingText = styled.h3`
   padding: 5%;
   border: 1.5px solid ${COLORS.MAIN};
   border-radius: 2%;
+  ${pc`
+  `}
+  ${sp`
+      margin: 20% 0;
+  `}
 `
